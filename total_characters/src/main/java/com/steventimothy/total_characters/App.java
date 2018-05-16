f8b44ff7a9a1eaf75e4bf38573c683be45456edd
Steven Timothy <stimothy@familysearch.org>
2018-05-16 11:49:49 -0600

    finished total_characters

--- total_characters/src/main/java/com/steventimothy/total_characters/App.java
+++ total_characters/src/main/java/com/steventimothy/total_characters/App.java
@@ -2,12 +2,18 @@ package com.steventimothy.total_characters;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+  public static void main(String[] args) {
+    StringBuilder str = new StringBuilder();
+    for (Integer i = 0; i < Integer.MAX_VALUE; ++i) {
+      try {
+        str.append(i);
+      }
+      catch (OutOfMemoryError error) {
+        System.out.println("This is how many characters a string can hold: " + i);
+        break;
+      }
     }
+  }
 }
